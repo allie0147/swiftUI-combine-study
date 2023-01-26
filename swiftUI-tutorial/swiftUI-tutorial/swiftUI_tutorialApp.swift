5eaba9dd615404ea353373f884543d7a2f3c7cf0
Allie Kim <allie0147@gmail.com>
2023-01-26 17:18:33 +0900

    Displaying data in a list

--- swiftUI-tutorial/swiftUI-tutorial/swiftUI_tutorialApp.swift
+++ swiftUI-tutorial/swiftUI-tutorial/swiftUI_tutorialApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct swiftUI_tutorialApp: App {
     var body: some Scene {
         WindowGroup {
-            CardView(scrum: DailyScrum(title: "Daily", attendees: [""], lengthInMinutes: 1, theme: .yellow))
+            MeetingView()
         }
     }
 }
