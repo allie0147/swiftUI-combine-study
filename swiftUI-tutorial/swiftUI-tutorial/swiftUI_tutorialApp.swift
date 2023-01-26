9c3fc61b67175abc598359dd4c3cad8fc492bd72
Allie Kim <allie0147@gmail.com>
2023-01-26 15:33:45 +0900

    Using stacks to arrange views

---
+++ swiftUI-tutorial/swiftUI-tutorial/swiftUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_tutorialApp.swift
+//  swiftUI-tutorial
+//
+//  Created by AllieKim on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
